2f91b92fb2e06130815e8fd34d7e0c090dc1be11
Manpreet-Singhh <manpreetsingh4438@gmail.com>
2024-05-17 20:56:07 +0530

    made changes

--- src/Components/EmployeeTable.jsx
+++ src/Components/EmployeeTable.jsx
@@ -4,22 +4,23 @@ import styles from './EmployeeTable.module.css';
 const EmployeeTable = () => {
   const [employees, setEmployees] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [loading, setLoading] = useState(true);
+//   const [loading, setLoading] = useState(true);
   const [error, setError] = useState('');
   const itemsPerPage = 10;
 
   const fetchEmployees = async () => {
     try {
       const response = await fetch('https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json');
-      if (!response.ok) {
-        throw new Error('Failed to fetch data');
-      }
+    //   if (!response.ok) {
+    //     throw new Error('Failed to fetch data');
+    //   }
       const data = await response.json();
       setEmployees(data);
-      setLoading(false);
+    //   setLoading(false);
     } catch (error) {
       setError('Failed to fetch data');
-      setLoading(false);
+      alert("Error fetching data");
+    //   setLoading(false);
     }
   };
 
@@ -54,9 +55,9 @@ const EmployeeTable = () => {
     ));
   };
 
-  if (loading) {
-    return <div className={styles['table-container']}>Loading...</div>;
-  }
+//   if (loading) {
+//     return <div className={styles['table-container']}>Loading...</div>;
+//   }
 
   if (error) {
     return <div className={styles['table-container']}>{error}</div>;
